b73710cdecbdc436538f98d830000ca832a87f99
BigHeadliner <bigheadliner@gmail.com>
2022-07-17 17:39:52 +0300

    smooth scroll add

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-$(function(){ 
+$(function(){  
+   
+  $(document).ready(function(){
+    $(".menu a ").on("click", function (event) {
+      //відміняєм стандартне опрацювання натиску ссилки
+      event.preventDefault();
+       
+      //забираєм ідентифікатор блоку з атрибута href 
+      var id  = $(this).attr('href'),
+  
+      //дізнаємся висоту від початку сторінки до блоку на який ссилається якір
+        top = $(id).offset().top;
+      
+      //анімуєм перехід на відстань - top за 1500 мс
+      $('body,html').animate({scrollTop: top}, 1500);
+    });
+  });
 
    
   $('.slider-blog__inner').slick({ 
